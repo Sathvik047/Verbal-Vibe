d199de8f807165df3abae45df692b9519594b4a3
Sathvik Thandra <sthandra2001@gmail.com>
2023-08-08 04:34:08 -0400

    Functions to prompt user for API key and save it in a file and to load the API key from the file

---
+++ T_S.py
@@ -0,0 +1,15 @@
+import json
+import http.client
+
+def get_api_key():
+    api_key = input("Please enter your API key: ")
+    with open("api_key.txt", "w") as file:
+        file.write(api_key)
+
+def load_api_key():
+    try:
+        with open("api_key.txt", "r") as file:
+            api_key = file.read().strip()
+        return api_key
+    except FileNotFoundError:
+        return None
